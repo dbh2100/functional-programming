7a90e9d5a230e1140922c48599f111c11964265a
dbh2100 <davidbharrison@gmail.com>
2026-05-21 21:47:34 -0400

    Adding general polynomial function

--- polynomials.hs
+++ polynomials.hs
@@ -1,3 +1,7 @@
 quadratic :: (Num a) => a -> a -> a -> (a -> a)
 quadratic a b c = applyQuadratic
     where applyQuadratic x = a*x^2 + b*x + c
+
+polynomial :: (Num a) => [a] -> a -> a
+polynomial [] _ = 0
+polynomial (n:ns) x = n * x^(length ns) + polynomial ns x
