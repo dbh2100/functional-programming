b3b4872c2ab888341dcc068aefb5a624bab82626
dbh2100 <davidbharrison@gmail.com>
2026-05-25 17:19:26 -0400

    Adding export function specifications

--- ChurchNumeral.hs
+++ ChurchNumeral.hs
@@ -1,3 +1,7 @@
+module ChurchNumeral
+( churchNumeral
+) where
+
 churchNumeral :: (Integral a) => a -> ((b -> b) -> b -> b)
 churchNumeral 0 f x = x
 churchNumeral n f x = f (churchNumeral (n-1) f x)
--- polynomials.hs
+++ polynomials.hs
@@ -1,3 +1,8 @@
+module Polynomials
+( quadratic
+, polynomial
+) where
+
 quadratic :: (Num a) => a -> a -> a -> (a -> a)
 quadratic a b c = applyQuadratic
     where applyQuadratic x = a*x^2 + b*x + c
