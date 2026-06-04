113b4552c1d60ebee2024aebd24156730bccbb4f
dbh2100 <davidbharrison@gmail.com>
2026-06-03 20:05:00 -0400

    Explicit import

--- ComplexFunctions.hs
+++ ComplexFunctions.hs
@@ -2,8 +2,8 @@ module ComplexFunctions
 ( mandelbrotSet
 ) where
 
-import Data.Complex
-import Data.List
+import Data.Complex ( Complex )
+import Data.List()
 
 mandelbrotSet :: Complex Float -> [Complex Float]
 mandelbrotSet c = iterate (\z -> z^2 + c) 0
