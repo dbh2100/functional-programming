627f8808e0fa854275f75405270932ef145c2664
dbh2100 <davidbharrison@gmail.com>
2026-06-23 10:50:50 -0400

    Adding comments to ComplexFunctions.hs

--- ComplexFunctions.hs
+++ ComplexFunctions.hs
@@ -8,6 +8,7 @@ import Data.Complex ( Complex )
 import Data.List()
 
 -- This function generates the Mandelbrot set for a given complex number c.
--- It returns an infinite list of complex numbers representing the iterations of the Mandelbrot function starting from 0. Each element in the list is computed by squaring the previous element and adding c.
+-- It returns an infinite list of complex numbers representing the iterations
+-- of the Mandelbrot function starting from 0. Each element in the list is computed by squaring the previous element and adding c.
 mandelbrotSet :: Complex Float -> [Complex Float]
 mandelbrotSet c = iterate (\z -> z^2 + c) 0
